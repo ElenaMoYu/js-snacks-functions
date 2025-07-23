128d130c64c828a41ca617ef4689fa51a0977c74
Elena <cavaldoro.elena@gmail.com>
2025-07-23 17:30:08 +0200

    feat: 1 done

---
+++ snack-funzioni/01_saluti/script.js
@@ -0,0 +1,12 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const nome = "Mario";
+
+// Dichiara la funzione qui.
+
+const sayHello = (nome) => `Ciao ${nome}`;
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(sayHello(nome));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
